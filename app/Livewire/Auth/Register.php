6d3037668b02372a3282229e196324b4cfb3a90e
cybernexus <raczdwork@proton.me>
2024-02-06 14:51:19 +0100

    Add registration validation & add vite to layout to load Tailwind assets.

--- app/Livewire/Auth/Register.php
+++ app/Livewire/Auth/Register.php
@@ -2,18 +2,45 @@
 
 namespace App\Livewire\Auth;
 
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class Register extends Component
 {
+    #[Validate]
     public $name;
+
+    #[Validate]
     public $username;
+
+    #[Validate]
     public $email;
+
+    #[Validate]
     public $password;
+
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'username' => 'required|string|max:255|unique:users,username',
+            'email' => 'required|email|max:255|unique:users,email',
+            'password' => 'required|string|min:6',
+        ];
+    }
 
     public function store()
     {
+        $validated = $this->validate();
+        dd($validated);
+    }
 
+    public function mount()
+    {
+        $this->name = '';
+        $this->username = '';
+        $this->email = '';
+        $this->password = '';
     }
 
     public function render()
--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -1,3 +1,38 @@
 <div>
 
+    <form wire:submit='store' class="max-w-md mx-auto">
+
+        <div class="relative">
+            <input type="text" wire:model='name' maxlength="10" class="w-full p-2 pr-10 border border-gray-300">
+            <p x-text="$wire.name.length + '/10'"
+                class="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
+            </p>
+        </div>
+
+        <div class="relative">
+            <input type="text" wire:model='username' maxlength="10" class="w-full p-2 mt-2 border border-gray-300">
+            <p x-text="$wire.username.length + '/10'"
+                class="absolute inset-y-0 right-0 flex items-center pr-3 mt-2 pointer-events-none">
+            </p>
+        </div>
+
+        <div class="relative">
+            <input type="email" wire:model='email' maxlength="10" class="w-full p-2 mt-2 border border-gray-300">
+            <p x-text="$wire.email.length + '/10'"
+                class="absolute inset-y-0 right-0 flex items-center pr-3 mt-2 pointer-events-none">
+            </p>
+        </div>
+
+        <div class="relative">
+            <input type="password" wire:model='password' maxlength="10" class="w-full p-2 mt-2 border border-gray-300">
+            <p x-text="$wire.password.length + '/10'"
+                class="absolute inset-y-0 right-0 flex items-center pr-3 mt-2 pointer-events-none">
+            </p>
+        </div>
+
+        <button class="mt-2">Regisztráció</button>
+    </form>
+
+
+
 </div>
